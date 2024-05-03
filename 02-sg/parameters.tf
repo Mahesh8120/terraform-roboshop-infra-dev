4d349da31ffc586cc90bde8940402cccd6cd30bc
Mahesh8120 <jonnalagaddamahi@gmail.com>
2024-05-03 14:17:11 +0530

    jenkins

--- 02-sg/parameters.tf
+++ 02-sg/parameters.tf
@@ -1,4 +1,4 @@
-resource "aws_ssm_parameter" "mongodb_sg_id" {
+resource "aws_ssm_parameter"" mongodb_sg_id" {
   name  = "/${var.project_name}/${var.environment}/mongodb_sg_id"
   type  = "String"
   value = module.mongodb_sg.sg_id
